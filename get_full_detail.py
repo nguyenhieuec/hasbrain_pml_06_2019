d5da907618ce79ed0d953df81fa6c6e4320696d4
Nguyenhieuqcf <nguyenhieuec@gmail.com>
2019-07-25 06:48:08 +0700

    intial staging crawl data

---
+++ get_full_detail.py
@@ -0,0 +1,46 @@
+
+import pandas as pd
+import numpy as np
+
+#internet package
+
+import urllib3
+from bs4 import BeautifulSoup  
+http = urllib3.PoolManager()
+
+#functional package
+import logging
+import requests
+from newspaper import Article
+from newspaper import fulltext
+from tqdm import tqdm
+from tqdm import tnrange, tqdm_notebook
+
+logging.basicConfig(filename="crawlfulltext.log",level=logging.ERROR)
+
+news_url = pd.read_csv("aapl.csv", usecols = range(0,2), encoding='latin-1')
+
+
+def get_details_from_url(df_to_get):
+    data_dicts = []
+    for url in tqdm(df_to_get['url'].values):
+        row_dict = {}
+        try:
+            article = Article(url)
+            article.download()
+            article.parse()
+            article.nlp()
+            row_dict['url'] = url
+            row_dict['fulltext'] = article.text
+            row_dict['summary'] = article.summary
+            row_dict['keywords'] = article.keywords
+            data_dicts.append(row_dict)
+        except Exception as e:
+#             logging.error()
+            continue
+    return data_dicts
+
+
+
+aapl = pd.DataFrame(get_details_from_url(news_url))
+aapl.to_csv("aapl.csv")
